fff690087de22769104a06cafc54065d0b4e84cd
Joseph Arias <arias.joseph.a@gmail.com>
2021-10-03 23:08:41 -0400

    implemented the Header component

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+// Decorator that marks a class as an Angular component 
+// and provides configuration metadata that determines 
+// how the component should be processed, instantiated, and used at runtime.
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html'
+})
+
+export class HeaderComponent {
+
+}
