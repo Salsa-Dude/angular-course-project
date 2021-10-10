e9a671d0d4a7fafaebdf9b8a9a0917794747701d
Joseph Arias <arias.joseph.a@gmail.com>
2021-10-10 14:25:11 -0400

    Managing Recipes in a Recipe Service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe-service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -12,13 +13,15 @@ export class RecipeListComponent implements OnInit {
   
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
-  recipes: Recipe[] = [
-    new Recipe('A test recipe', 'this is a test', 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/9/26/0/FNK_Tuscan-Chicken-Skillet_H2_s4x3.jpg.rend.hgtvcom.616.462.suffix/1537973085542.jpeg'), new Recipe('Another test recipe', 'this is a test', 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/9/26/0/FNK_Tuscan-Chicken-Skillet_H2_s4x3.jpg.rend.hgtvcom.616.462.suffix/1537973085542.jpeg')
-  ];
+  recipes: Recipe[] = []
+  //   new Recipe('A test recipe', 'this is a test', 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/9/26/0/FNK_Tuscan-Chicken-Skillet_H2_s4x3.jpg.rend.hgtvcom.616.462.suffix/1537973085542.jpeg'), new Recipe('Another test recipe', 'this is a test', 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/9/26/0/FNK_Tuscan-Chicken-Skillet_H2_s4x3.jpg.rend.hgtvcom.616.462.suffix/1537973085542.jpeg')
+  // ];
 
-  constructor() { }
+  // inject our service
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit(): void {
+    this.recipes = this.recipeService.getRecipes();
   }
 
   onRecipeSelected(recipe: Recipe) {
--- src/app/recipes/recipe-service.ts
+++ src/app/recipes/recipe-service.ts
@@ -1,5 +1,14 @@
+import { Recipe } from "./recipe.model";
 
 // a Service is simply a TypeScript class
 export class RecipeService {
+
+  private recipes: Recipe[] = [
+    new Recipe('A test recipe', 'this is a test', 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/9/26/0/FNK_Tuscan-Chicken-Skillet_H2_s4x3.jpg.rend.hgtvcom.616.462.suffix/1537973085542.jpeg'), new Recipe('Another test recipe', 'this is a test', 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/9/26/0/FNK_Tuscan-Chicken-Skillet_H2_s4x3.jpg.rend.hgtvcom.616.462.suffix/1537973085542.jpeg')
+  ];
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
 
 }
