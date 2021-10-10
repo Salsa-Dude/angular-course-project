b8d267b26c76178a8607b2f6fa37cbadd79a8497
Joseph Arias <arias.joseph.a@gmail.com>
2021-10-10 14:43:22 -0400

    Using a Service for Cross-Component Communication

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { RecipeService } from '../../recipe-service';
 
 import { Recipe } from '../../recipe.model';
 
@@ -12,17 +13,21 @@ export class RecipeItemComponent implements OnInit {
   // signifies that the property can receive its value from its parent component.
   @Input() recipe: Recipe;
 
+  constructor(private recipeService: RecipeService){}
+
   // The child component uses the @Output() property to raise an event to notify the parent of the change.
   // To raise an event, an @Output() must have the type of EventEmitter
-  @Output() recipeSelected = new EventEmitter<void>();
+  // @Output() recipeSelected = new EventEmitter<void>();
 
-  constructor() { }
 
   ngOnInit(): void {
   }
 
   onSelected() {
-    this.recipeSelected.emit();
+    // this.recipeSelected.emit();
+
+    // inject
+    this.recipeService.recipeSelected.emit(this.recipe);
   }
 
 }
--- src/app/recipes/recipe-service.ts
+++ src/app/recipes/recipe-service.ts
@@ -1,7 +1,10 @@
+import { EventEmitter } from "@angular/core";
 import { Recipe } from "./recipe.model";
 
 // a Service is simply a TypeScript class
 export class RecipeService {
+
+  recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
     new Recipe('A test recipe', 'this is a test', 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/9/26/0/FNK_Tuscan-Chicken-Skillet_H2_s4x3.jpg.rend.hgtvcom.616.462.suffix/1537973085542.jpeg'), new Recipe('Another test recipe', 'this is a test', 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/9/26/0/FNK_Tuscan-Chicken-Skillet_H2_s4x3.jpg.rend.hgtvcom.616.462.suffix/1537973085542.jpeg')
